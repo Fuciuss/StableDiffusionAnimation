afb0e726848de6e60504569cedd395ac5d77dca1
Rees <reespawson@gmail.com>
2023-07-12 07:46:09 +0000

    first

---
+++ app.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+class StableDiffusionVideo():
+
+    def __init__(
+        self,
+        api_url: str
+    ):
+        
+        self.api_url = api_url
+        self.video_frames = self.get_video_frames()
+
+
+    def get_video_frames():
+        pass
+
+
+    def generate_random_samples(test_frames=5):
+        pass
+
+
+
