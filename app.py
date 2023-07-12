8660029995b9635da90043747e5331ef36604e76
Rees <reespawson@gmail.com>
2023-07-12 07:53:36 +0000

    x

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-
-
+import cv2
+import os
 
 
 
@@ -7,11 +7,13 @@ class StableDiffusionVideo():
 
     def __init__(
         self,
-        api_url: str
+        api_url: str,
+        video_location: str,
     ):
         
         self.api_url = api_url
-        self.video_frames = self.get_video_frames()
+        self.video_location = video_location
+        self.video_frames = self.get_video_frames(video_location)
 
 
     def get_video_frames():
@@ -20,6 +22,20 @@ class StableDiffusionVideo():
 
     def generate_random_samples(test_frames=5):
         pass
+
+
+    
+    def generate_video():
+        pass
+
+
+if __name__ == "__main__":
+    print('in main')
+
+
+
+
+
 
 
 
